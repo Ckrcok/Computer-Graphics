4c7b3364681487cf427303dd7558d270cd9e719b
Dor <dorzairi@ymail.com>
2022-01-26 09:35:46 -0500

    added week 3

---
+++ week 3/ComponentFramework22.0.0/ComponentFramework/Scene0.cpp
@@ -0,0 +1,95 @@
+#include <glew.h>
+#include <iostream>
+#include <SDL.h>
+#include "Debug.h"
+#include "Scene0.h"
+#include "MMath.h"
+#include "Debug.h"
+#include "Actor.h"
+#include "Mesh.h"
+#include "Shader.h"
+
+Scene0::Scene0() :sphere(nullptr), shader{ nullptr }, mesh{nullptr}  {
+	Debug::Info("Created Scene0: ", __FILE__, __LINE__);
+}
+
+Scene0::~Scene0() {
+	Debug::Info("Deleted Scene0: ", __FILE__, __LINE__);
+}
+
+bool Scene0::OnCreate() {
+	Debug::Info("Loading assets Scene0: ", __FILE__, __LINE__);
+	sphere = new Actor(nullptr);
+	sphere->OnCreate();
+
+	mesh = new Mesh(nullptr, "meshes/Sphere.obj");
+	mesh->OnCreate();
+
+	shader = new Shader(nullptr, "shaders/phongVert.glsl", "shaders/phongFrag.glsl");
+	shader->OnCreate();
+	if (shader->OnCreate() == false){
+		std::cout << "Shader Failed..";
+	}
+
+	projectionMatrix = MMath::perspective(45.0f, (16.0f/9.0f), 0.5f, 100.f );
+	viewMatrix = MMath::lookAt(Vec3(0.0f, 0.0f, 5.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 1.0f, 0.0f));
+	modelMatrix.loadIdentity();
+	lightPos = Vec3(0.0f, 0.0f, 8.0f);
+	return true;
+}
+
+void Scene0::OnDestroy() {
+	Debug::Info("Deleting assets Scene0: ", __FILE__, __LINE__);
+	
+	sphere->OnDestroy();
+	delete sphere;
+
+	shader->OnDestroy();
+	delete shader;
+
+	mesh->OnDestroy();
+	delete mesh;
+
+}
+
+void Scene0::HandleEvents(const SDL_Event &sdlEvent) {
+	switch( sdlEvent.type ) {
+    case SDL_KEYDOWN:
+		break;
+
+	case SDL_MOUSEMOTION:          
+		break;
+
+	case SDL_MOUSEBUTTONDOWN:              
+		break; 
+
+	case SDL_MOUSEBUTTONUP:            
+	break;
+
+	default:
+		break;
+    }
+}
+
+void Scene0::Update(const float deltaTime) {
+	
+}
+
+void Scene0::Render() const {
+	glEnable(GL_DEPTH_TEST);
+	/// Clear the screen
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glUseProgram(shader->GetProgram());
+	glUniformMatrix4fv(shader->GetUniformID("projectionMatrix"), 1, GL_FALSE, projectionMatrix);
+	glUniformMatrix4fv(shader->GetUniformID("viewMatrix"), 1, GL_FALSE, viewMatrix);
+	glUniformMatrix4fv(shader->GetUniformID("modelMatrix"), 1, GL_FALSE, modelMatrix);
+	glUniform3fv(shader->GetUniformID("lightPos"), 1, lightPos);
+	mesh->Render(GL_TRIANGLE_FAN);
+	glUseProgram(0);
+}
+
+
+
+	
